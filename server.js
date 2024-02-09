1fcfa54459d48d471c8e3ab7c05ab4a20b92ce0f
houcine1amraoui <houcine1amraoui@gmail.com>
2024-02-09 18:58:55 +0100

    push project to github

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+
+app.use("/css", express.static(path.resolve(__dirname, "pages", "css")));
+app.use("/js", express.static(path.resolve(__dirname, "pages", "js")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "pages", "index.html"));
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
